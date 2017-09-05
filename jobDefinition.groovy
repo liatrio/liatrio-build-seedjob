8362eb928445d65d01c2b0f68fff89f19c786637
Benjamin Stein <stein321@gmail.com>
2017-09-05 12:24:16 -0700

    Update jobDefinition.groovy

--- jobDefinition.groovy
+++ jobDefinition.groovy
@@ -35,6 +35,7 @@ folder = folder("pipelineJobs") {
                             git {
                                 id('73a88996-5cc1-4624-8fb5-8a148776e26e')
                                 remote('https://github.com/liatrio/pipeline-library.git')
+                            }
                         }
                     }
                 }
